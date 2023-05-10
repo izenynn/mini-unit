570ad0dc6b2c91c78c44f6a40f85dbc981b5b45a
izenynn <me@izenynn.com>
2023-05-10 16:20:25 +0200

    feat: initial version

--- README.md
+++ README.md
@@ -6,7 +6,47 @@ A simple unit test framework for C in C.
 
 ## Usage
 
-`TODO`
+For unit testing I don't like to have the test files in a `tests/` folder, I prefer to put them next to the respective `.c` so I have both files at hand easily.
+
+So, I usually add the following to my `Makefile`, take it as a reference for your own needs:
+
+```Makefile
+# ... CC, CFLAGS, RM, MKDIR, ...
+
+SRC_DIR := src
+OBJ_DIR := obj
+TEST_DIR := tests
+
+SRC_FILES := # ...
+TEST_SRC_FILES := # ...
+
+OBJ_FILES := $(SRC:.c=.o)
+TEST_OBJ_FILES = $(TEST_SRC:.c:.o)
+
+SRC := $(addprefix $(SRC_DIR)/, $(SRC_FILES))
+OBJ := $(addprefix $(OBJ_DIR)/, $(OBJ_FILES))
+TEST_SRC := $(addprefix $(TEST_DIR)/, $(TEST_SRC_FILES))
+TEST_OBJ := $(addprefix $(OBJ_DIR)/, $(TEST_OBJ_FILES))
+
+TEST_BIN = $(patsubst $(SRC_DIR)/%,$(TEST_DIR)/%,$(TEST_SRC:.c=.test))
+
+# ...
+
+test: $(TEST_BIN)
+    for test in $^; do ./$$test; done
+
+$(TEST_DIR)/%_test.test: $(OBJ_DIR)/%_test.o $(OBJ) | $(TEST_DIR)
+    $(CC) $(CFLAGS) -o $@ $^
+
+$(TEST_DIR):
+    $(MKDIR) $@
+
+clean:
+    $(RM) $(OBJ_DIR) $(TEST_DIR)
+    # ...
+
+.PHONY: test #...
+```
 
 ##
 
---
+++ miniunit.h
@@ -0,0 +1,91 @@
+#ifndef MINIUNIT_MINIUNIT_H_
+#define MINIUNIT_MINIUNIT_H_
+
+#include "colors.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+static struct {
+  unsigned int total_tests;
+  unsigned int passed_tests;
+  unsigned int failed_tests;
+  bool current_test_success;
+} test_stats = { 0, 0, 0, true };
+
+#define ASSERT(test, message) do { \
+    if (!(test)) { \
+      printf(C_RED "FAIL:" C_OFF " %s\n", message); \
+      test_stats.current_test_success = false; \
+    } \
+  } while (0)
+
+#define RUN_TEST(test) do { \
+    ++test_stats.total_tests; \
+    test_stats.current_test_success = true; \
+    test(); \
+    if (test_stats.current_test_success) { \
+      ++test_stats.passed_tests; \
+    } else { \
+      ++test_stats.failed_tests; \
+    } \
+  } while (0)
+
+// #define BEGIN() do { \
+//     ; \
+//   } while (0)
+
+#define WIDTH (int)50
+// we'll substract:
+// - the __FILE__ len
+// - 2 for the spaces on the sides
+// - 2 for the "ok/ko" text
+
+#define END() do { \
+  int file_width = strlen(__FILE__) >= WIDTH - 4 \
+      ? WIDTH - 4 \
+      : strlen(__FILE__); \
+  int dots_width = strlen(__FILE__) >= WIDTH - 4 \
+      ? 0 \
+      : WIDTH - strlen(__FILE__) - 4; \
+  printf( \
+      "%.*s %.*s", \
+      file_width, \
+      __FILE__, \
+      dots_width, \
+      ".................................................."); \
+  if (test_stats.failed_tests == 0) { \
+    printf(" " C_GREEN "ok" C_OFF); \
+  } else { \
+    printf(" " C_RED "ko" C_OFF); \
+  } \
+  printf( \
+      "     (" C_GREEN "%d" C_OFF "/" C_RED "%d" C_OFF ")\n", \
+      test_stats.passed_tests, \
+      test_stats.failed_tests); \
+  return ((test_stats.failed_tests == 0) ? 0 : 1); \
+} while (0)
+
+#define TEST_CASE(test) \
+  void test(void)
+
+#define TEST_MAIN \
+  int main(void)
+
+#endif // MINIUNIT_MINIUNIT_H_
+
+/*
+======================================================================
+============================================================================
+================================================================================
+
+printf("==================================================\n"); \
+printf("test summary"); \
+printf("==================================================\n"); \
+printf("# TOTAL: %d\n", test_stats.total_tests); \
+printf("# PASS:  %d\n", test_stats.passed_tests); \
+printf("# FAIL:  %d\n", test_stats.failed_tests); \
+printf("==================================================\n"); \
+
+*/
