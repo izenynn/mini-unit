67c2f3a9ce778b6bedacded8d7ab9396f47152d4
izenynn <me@izenynn.com>
2023-05-13 15:17:32 +0200

    style: change style

--- miniunit.h
+++ miniunit.h
@@ -5,11 +5,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define C_OFF   "\033[0m"
-#define C_GREEN "\033[0;32m"
-#define C_RED   "\033[0;31m"
+#define C_OFF	"\033[0m"
+#define C_GREEN	"\033[0;32m"
+#define C_RED	"\033[0;31m"
 
-/* __FILE__ is part of the C PreProcessor standard... but who knows.
+/*
+ * __FILE__ is part of the C PreProcessor standard... but who knows.
  * also, this way you can also change the FILENAME displayed on the output just
  * be defining FILENAME on the command line with `-DFILENAME=...`
  */
@@ -17,7 +18,8 @@
 #define FILENAME ((__FILE__) ? (__FILE__) : ("unknown file"))
 #endif // FILENAME
 
-/* if you define this in your own Makefile, make sure its declared as a string,
+/*
+ * If you define this in your own Makefile, make sure its declared as a string,
  * the quotes are very important:
  * CFLAGS += -DRELATIVE_PATH="\"$(RELATIVE_PATH)\""
  */
@@ -28,72 +30,70 @@
 #endif // RELATIVE_PATH
 
 static struct {
-  unsigned int  total_tests;
-  unsigned int  passed_tests;
-  unsigned int  failed_tests;
-  bool          current_test_success;
-  const char*   current_test_name;
+	unsigned int	total_tests;
+	unsigned int	passed_tests;
+	unsigned int	failed_tests;
+	bool		current_test_success;
+	const char*	current_test_name;
 } test_stats = { 0, 0, 0, true, NULL };
 
-#define ASSERT(test, message) do { \
-    if (!(test)) { \
-      printf( \
-          C_RED "FAIL" C_OFF ": %s: %s\n", \
-          test_stats.current_test_name, \
-          message); \
-      test_stats.current_test_success = false; \
-    } \
-  } while (0)
+#define ASSERT(test, message) do {					\
+		if (!(test)) {						\
+			printf(C_RED "FAIL" C_OFF ": %s: %s\n",		\
+			       test_stats.current_test_name,		\
+			       message);				\
+			test_stats.current_test_success = false;	\
+		}							\
+} while (0)
 
-#define RUN_TEST(test) do { \
-    ++test_stats.total_tests; \
-    test_stats.current_test_success = true; \
-    test_stats.current_test_name = #test; \
-    test(); \
-    if (test_stats.current_test_success) { \
-      ++test_stats.passed_tests; \
-    } else { \
-      ++test_stats.failed_tests; \
-    } \
-  } while (0)
+#define RUN_TEST(test) do {						\
+		++test_stats.total_tests;				\
+		test_stats.current_test_success = true;			\
+		test_stats.current_test_name = #test;			\
+		test();							\
+		if (test_stats.current_test_success)			\
+			++test_stats.passed_tests;			\
+		else							\
+			++test_stats.failed_tests;			\
+} while (0)
 
 
-/* for the actual width we'll substract:
+/*
+ * For the actual width we'll substract:
  * - the __FILE__ len (with RELATIVE_PATH substracted)
  * - 2 for the spaces on the sides
  * - 2 for the "ok/ko" text
  */
 #define WIDTH (int)50
-#define END() do { \
-  char* filename_ptr = FILENAME; \
-  int file_width = (int)strlen(filename_ptr) - RELATIVE_PATH_LEN >= WIDTH - 4 \
-      ? WIDTH - 4 \
-      : (int)strlen(filename_ptr) + RELATIVE_PATH_LEN; \
-  int dots_width = (int)strlen(filename_ptr) - RELATIVE_PATH_LEN >= WIDTH - 4 \
-      ? 0 \
-      : WIDTH - (int)strlen(filename_ptr) + RELATIVE_PATH_LEN - 4; \
-  printf( \
-      "%.*s %.*s", \
-      file_width, \
-      filename_ptr + RELATIVE_PATH_LEN, \
-      dots_width, \
-      ".................................................."); \
-  if (test_stats.failed_tests == 0) { \
-    printf(" " C_GREEN "ok" C_OFF); \
-  } else { \
-    printf(" " C_RED "ko" C_OFF); \
-  } \
-  printf( \
-      "     (" C_GREEN "%d" C_OFF "/" C_RED "%d" C_OFF ")\n", \
-      test_stats.passed_tests, \
-      test_stats.failed_tests); \
-  return ((test_stats.failed_tests == 0) ? 0 : 1); \
+#define END() do {							\
+	char* ptr = FILENAME;						\
+	int file_width =						\
+		(int)strlen(ptr) - RELATIVE_PATH_LEN >= WIDTH - 4	\
+		? WIDTH - 4						\
+		: (int)strlen(ptr) + RELATIVE_PATH_LEN;			\
+	int dots_width =						\
+		(int)strlen(ptr) - RELATIVE_PATH_LEN >= WIDTH - 4	\
+		? 0							\
+		: WIDTH - (int)strlen(ptr) + RELATIVE_PATH_LEN - 4;	\
+	printf("%.*s %.*s",						\
+	      file_width,						\
+	      ptr + RELATIVE_PATH_LEN,					\
+	      dots_width,						\
+	      "..................................................");	\
+	if (test_stats.failed_tests == 0)				\
+		printf(" " C_GREEN "ok" C_OFF);				\
+	else								\
+		printf(" " C_RED "ko" C_OFF);				\
+	printf("     (" C_GREEN "%d" C_OFF "/" C_RED "%d" C_OFF ")\n",	\
+	      test_stats.passed_tests,					\
+	      test_stats.failed_tests);					\
+	return ((test_stats.failed_tests == 0) ? 0 : 1);		\
 } while (0)
 
 #define TEST_CASE(test) \
-  static void test(void)
+	static void test(void)
 
 #define TEST_MAIN \
-  int main(void)
+	int main(void)
 
 #endif // MINIUNIT_MINIUNIT_H_
